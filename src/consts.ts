71dc089261a6bb8b6917c5beda474d05d00d0816
keroway <4470654+keroway@users.noreply.github.com>
2023-12-31 08:39:42 +0900

    homeとaboutの記載、自作記事への差し替え他

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'keroway Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'keroway 技術メモ';
+export const SITE_DESCRIPTION = 'QiitaやZennに投稿するまででもない、ちょっとした内容を書いていきます。';
