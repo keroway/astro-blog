e9b7513fd03026144e55cb30b84de32006306116
keroway <kurokawa.y@gmail.com>
2025-10-28 14:55:34 +0900

    日本語スラッグのエンコードを修正

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,14 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	const toEncodedSlug = (slug) => slug.split('/').map((segment) => encodeURIComponent(segment)).join('/');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/blog/${toEncodedSlug(post.slug)}/`,
 		})),
 	});
 }
